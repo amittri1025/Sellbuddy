070e036cb1b92fff7b19930f70389b6ad84813ac
Amit Tripathi <tripathiamit2021@gmail.com>
2022-12-18 18:04:49 +0530

    added chat functionaliy

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,23 +1,27 @@
-import deal from '../images/deal.jpg'
-
-
+import deal from "../images/deal.jpg";
+import { Link } from "react-router-dom";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function Hero() {
-    return (  
-        <>
-          <div class="container-fluid">
-
-            <div className="text-container p-5">
-                <h1 >Sell Your Stuff on Chat | In real time</h1>
-                <p>It could be anything from furnitures to Devices to Pets, ANYTHING !!!</p>
-                <h2>And find potential customers in your area</h2>
-                <img src={deal} alt="" />
-
-            </div>
-            
-            
-          </div>  
-        </>
-    );
+  return (
+    <>
+      <div class="container-fluid">
+        <div className="text-container p-5">
+          <h1>Sell Your Stuff on Chat | In real time</h1>
+          <p>
+            It could be anything from furnitures to Devices to Pets, ANYTHING
+            !!!
+          </p>
+          <h2>And find potential customers in your area</h2>
+          <img src={deal} alt="" />
+          <Link className="text-white d-block py-5" to="/messages">
+            <button type="button" class="btn btn-primary btn-lg">
+              Start Selling
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Hero;
--- client/src/Pages/Messages.jsx
+++ client/src/Pages/Messages.jsx
@@ -1,9 +1,42 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+
+import 'stream-chat-react/dist/css/v2/index.css';
+
+const chatClient = new StreamChat('h4vajbr47v4k');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoid2hpdGUtZGF3bi00In0.O8XK2iH1Lx1_8YtQWbdLohbM72_HtgaFaBZawBugmGM';
+
+chatClient.connectUser(
+  {
+    id: 'white-dawn-4',
+    name: 'Amit',
+    image: 'https://getstream.io/random_png/?id=white-dawn-4&name=white-dawn-4',
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'custom_channel_id', {
+  // add as many custom fields as you'd like
+  image: 'https://www.drupal.org/files/project-images/react.png',
+  name: 'Sell Furniture',
+  members: ['white-dawn-4'],
+});
+
+
 function Messages() {
     return ( 
     <>
-        <h2>
-            Messages
-        </h2>
+    <Chat client={chatClient} theme='str-chat__theme-light'>
+        <Channel channel={channel}>
+        <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+        </Window>
+        <Thread />
+        </Channel>
+    </Chat>
     </>
      );
 }
